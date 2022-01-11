3b388362e605aacfd55f27d0410cda264cf8c94c
SISTLA SAI PALLAVI <83531350+sai-pallavi-2003@users.noreply.github.com>
2022-01-11 13:03:33 +0530

    Create ReversingString.cpp

---
+++ ReversingString.cpp
@@ -0,0 +1,20 @@
+
+// C++ program to print reverse of a string
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to reverse a string
+void reverse(string str)
+{
+   for (int i=str.length()-1; i>=0; i--)
+      cout << str[i];
+}
+ 
+// Driver code
+int main(void)
+{
+    string s ;
+    cin>>s;
+    reverse(s);
+    return (0);
+}
